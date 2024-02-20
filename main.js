109e2b56f2535f5edc53486b9a2f13da3abf1181
Cisco29 <castro.francisco@live.fr>
2024-02-20 12:22:28 +0100

    petit fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var type = new Typed(".text", {
-    strings: ["Frontend Developer", "Backend Developer", "Web Developer"],
+    strings: ["Frontend Developer", "Backend Developer", "Web Developer", "Fan d'Itachi Uchiha"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
