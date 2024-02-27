388c301ff15170c8a1307ddcc136ab34bc24949a
Cisco29 <castro.francisco@live.fr>
2024-02-27 14:46:45 +0100

    Continuation mise en français

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var type = new Typed(".text", {
-    strings: ["Développeur Frontend", "Développeur Backend", "Developpeur Mobile", "Fan d'Itachi Uchiha"],
+    strings: ["Développeur Frontend", "Développeur Backend", "Développeur Mobile", "Fan d'Itachi Uchiha"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
